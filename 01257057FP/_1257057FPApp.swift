a9e5f241e113ff0b19bbfa5cd56e6f0bbb72b5bc
ken204 <Kenny20041223@gmail.com>
2025-12-11 15:53:25 +0800

    Initial Commit

---
+++ 01257057FP/_1257057FPApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1257057FPApp.swift
+//  01257057FP
+//
+//  Created by user05 on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct _1257057FPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
